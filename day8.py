a93319f53fbe4e048afb4eb00e34032e9a976715
Gioacchino Castorio <gioacchino.castorio@gmail.com>
2023-12-08 15:46:10 +0000

    day8

--- day8.py
+++ day8.py
@@ -1,7 +1,82 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from math import lcm
+from typing import Generator, NamedTuple
+from re import match
+
+class Node(NamedTuple):
+    name: str
+    left: str
+    right: str
+
+    @staticmethod
+    def loads(s: str) -> Node:
+        groups = match(r"(?P<name>[A-Z]+) = \((?P<left>[A-Z]+), (?P<right>[A-Z]+)\)", s)
+        if not groups:
+            raise ValueError(f'not valid input --> {s}')
+        return Node(**groups.groupdict())
+    
+    def __eq__(self, o: Node) -> bool:
+        return self.name == o.name
+    
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+
+@dataclass
+class Map:
+    steps: str
+    start_point: Node
+    nodes: dict[str, Node]
+
+    def get_next_step_generator(self) -> Generator[str, None, None]:
+        while True:
+            for i in self.steps:
+                yield i
+
+    @staticmethod
+    def loads(lines: list[str]) -> Map:
+        steps = lines[0]
+        nodes = {}
+        start_point = None
+        for l in lines[1:]:
+            if not len(l):
+                continue
+            n = Node.loads(l.strip())
+            if not start_point:
+                start_point = n
+            nodes[n.name] = n
+        return Map(steps, start_point, nodes) # type: ignore
+    
+    def compute_path(self, start_point: Node | None = None, does_ends_by_z: bool = False) -> list[str]:
+        path = []
+        current_node = start_point if start_point else self.start_point
+        step_gen = self.get_next_step_generator()
+
+        while True:
+            if does_ends_by_z and current_node.name.endswith('Z'):
+                break
+            if current_node.name == 'ZZZ':
+                break
+            step = next(step_gen)
+            path.append(current_node.name)
+            next_node_name = current_node.left if step == 'L' else current_node.right
+            current_node = self.nodes[next_node_name]
+        
+        return path
+    
+    def get_parallel_start_nodes(self) -> list[Node]:
+        return [self.nodes[sn] for sn in self.nodes if sn.endswith('A')]
+    
+    def get_parallel_paths_lenght(self) -> int:
+        return lcm(*[len(self.compute_path(sp, does_ends_by_z=True)) for sp in self.get_parallel_start_nodes()])
+
 
 if __name__ == '__main__':
 
     with open('./input.txt') as f:
         lines = [line.strip() for line in f.readlines()]
+
+    print(f'part1 total = {len(Map.loads(lines).compute_path())}')
+    print(f'part2 total = {Map.loads(lines).get_parallel_paths_lenght()}')
--- test_day8.py
+++ test_day8.py
@@ -1,7 +1,107 @@
+import pytest
+
 from day8 import *
 
-def test_parse_line():
-    pass
+def test_get_circular_directions():
+    m = Map('LLR', None ,  {}) # type: ignore
+    last = ''
+    ns_gen = m.get_next_step_generator()
+    for _ in range(6):
+        last = next(ns_gen)
+    assert last == 'R'
+
+def test_parse_node_string():
+    expected = Node('AAA', left='BBB', right='CCC')
+    assert Node.loads('AAA = (BBB, CCC)') == expected
+
+    expected = Node('ZZZ', left='ZZZ', right='ZZZ')
+    assert Node.loads('ZZZ = (ZZZ, ZZZ)') == expected
+
+def test_parse_wrong_node_string():
+
+    with pytest.raises(ValueError):
+        Node.loads('aaa = (aaa, aaa)')
+
+    with pytest.raises(ValueError):
+        Node.loads('aaa = (aaa aaa)')
+    
+    with pytest.raises(ValueError):
+        Node.loads('aaa = (aaa, aaa')
+
+def test_parse_maps_string():
+    map_str = """RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)"""
+
+    m = Map.loads(map_str.splitlines())
+
+    assert m.steps == 'RL'
+    assert len(m.nodes) == 7
+    assert m.nodes['AAA'] == Node('AAA', left='BBB', right='CCC')
+    assert m.nodes['ZZZ'] == Node('ZZZ', left='ZZZ', right='ZZZ')
+
+
+def test_compute_sample_path():
+    map_str = """RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)"""
+
+    m = Map.loads(map_str.splitlines())
+    assert len(m.compute_path()) == 2
+    assert m.compute_path() == 'AAA CCC'.split()
+
+def test_count_path_lenght_circular():
+    map_str = """LLR
+
+    AAA = (BBB, BBB)
+    BBB = (AAA, ZZZ)
+    ZZZ = (ZZZ, ZZZ)"""
+
+    m = Map.loads(map_str.splitlines())
+    assert len(m.compute_path()) == 6
+
+
+def test_find_paralles_start_nodes():
+    map_str = """LR
+
+    KKA = (KKB, XXX)
+    KKB = (XXX, KKZ)
+    KKZ = (KKB, XXX)
+    JJA = (JJB, XXX)
+    JJB = (JJC, JJC)
+    JJC = (JJZ, JJZ)
+    JJZ = (JJB, JJB)
+    XXX = (XXX, XXX)"""
+
+    assert [n.name for n in Map.loads(map_str.splitlines()).get_parallel_start_nodes()] == ['KKA', 'JJA']
+
+def test_get_parallel_paths_lenght():
+    map_str = """LR
+
+    KKA = (KKB, XXX)
+    KKB = (XXX, KKZ)
+    KKZ = (KKB, XXX)
+    JJA = (JJB, XXX)
+    JJB = (JJC, JJC)
+    JJC = (JJZ, JJZ)
+    JJZ = (JJB, JJB)
+    XXX = (XXX, XXX)"""
+
+    m = Map.loads(map_str.splitlines())
+    assert m.get_parallel_paths_lenght() == 6
+
 
 def test_compute_sample_result():
     pass
