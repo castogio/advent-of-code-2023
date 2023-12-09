5d61007b117b91978805f7de67b84b73c087c65c
Gioacchino Castorio <gioacchino.castorio@gmail.com>
2023-12-09 23:47:35 +0000

    day9 part 2

--- day9.py
+++ day9.py
@@ -2,12 +2,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Callable
 
-def op_consecutive_pairs(lst: list[int], op: Callable) -> list[int]:
-    result = []
-    for i in range(len(lst)-1):
-        result.append(op(lst[i], lst[i+1]))
-    return result
-
 @dataclass
 class Reading:
     values: list[int]
@@ -16,18 +10,45 @@ class Reading:
         rows = [self.values]
         current = self.values
 
+        def op_consecutive_pairs(lst: list[int]) -> list[int]:
+            result = []
+            for i in range(len(lst)-1):
+                result.append(lst[i] - lst[i+1])
+            return result
+
         while current[0] != 0:
-            next_row = op_consecutive_pairs(current, int.__sub__)
+            next_row = op_consecutive_pairs(current)
             rows.append(next_row)
             current = next_row
 
         rows = rows[::-1]
-
         return sum(r[0] for r in rows[1::])
     
+    def get_part2_prediction(self) -> int:
+        rows = [self.values]
+        current = self.values
+
+        def op_consecutive_pairs(lst: list[int]) -> list[int]:
+            result = []
+            for i in range(len(lst)-1):
+                result.append(lst[i+1] - lst[i])
+            return result
+
+        while any(i != 0 for i in current):
+            next_row = op_consecutive_pairs(current)
+            rows.append(next_row)
+            current = next_row
+
+        rows = rows[::-1]
+        res = 0
+        for r in rows[1::]:
+            res = r[0] - res
+        return res
+    
     @staticmethod
-    def loads(s: str) -> Reading:
-        return Reading([int(r) for r in s.strip().split()[::-1]])
+    def loads(s: str, is_reversed: bool = True) -> Reading:
+        read_seq = s.strip().split()[::-1] if is_reversed else s.strip().split()
+        return Reading([int(r) for r in read_seq])
 
 if __name__ == '__main__':
 
@@ -35,3 +56,4 @@ if __name__ == '__main__':
         lines = [line.strip() for line in f.readlines()]
 
     print(f'part1 total = {sum(Reading.loads(l.strip()).get_part1_prediction() for l in lines if len(l))}')
+    print(f'part2 total = {sum(Reading.loads(l.strip(), False).get_part2_prediction() for l in lines if len(l))}')
--- test_day9.py
+++ test_day9.py
@@ -3,17 +3,24 @@ import pytest
 from day9 import *
 
 
-def test_op_consecutive_pairs():
-    assert op_consecutive_pairs([15, 12, 9, 6, 3, 0], int.__sub__) == [3, 3, 3, 3, 3]
-    assert op_consecutive_pairs([3, 3, 3, 3, 3], int.__sub__) == [0, 0, 0, 0]
+# def test_op_consecutive_pairs():
+#     assert op_consecutive_pairs([15, 12, 9, 6, 3, 0], int.__sub__) == [3, 3, 3, 3, 3]
+#     assert op_consecutive_pairs([3, 3, 3, 3, 3], int.__sub__) == [0, 0, 0, 0]
 
 def test_reading_loads():
     assert Reading.loads('0 3 6 9 12 15') == Reading([15, 12, 9, 6, 3, 0])
+    assert Reading.loads('0 3 6 9 12 15', is_reversed=False) == Reading([0, 3, 6, 9, 12, 15])
 
 def test_reading_get_part1_prediction():
     assert Reading([15, 12, 9, 6, 3, 0]).get_part1_prediction() == 18
     assert Reading([21, 15, 10, 6, 3, 1]).get_part1_prediction() == 28
     assert Reading([45, 30, 21, 16, 13, 10]).get_part1_prediction() == 68
+
+def test_reading_get_part2_prediction():
+    assert Reading([10, 13, 16, 21, 30, 45]).get_part2_prediction() == 5
+    assert Reading.loads('0 3 6 9 12 15', False).get_part2_prediction() == -3
+    assert Reading.loads('1 3 6 10 15 21', False).get_part2_prediction() == 0
+    
 
 def test_compute_sample_p1_total():
     sample = '''
